9f1ef7645c03c3f6a74c6951ceb682032e911f1e
Larry E. Godwin <developer.godwinlarry@gmail.com>
2019-03-02 13:30:42 +0000

    Add new main2activity

--- Week1/app/src/main/java/com/lostcodes/myfirstapplication/MainActivity.java
+++ Week1/app/src/main/java/com/lostcodes/myfirstapplication/MainActivity.java
@@ -1,13 +1,42 @@
 package com.lostcodes.myfirstapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_MESSAGE = "My_App";
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+
     }
+
+    private void addListenerOnButton(){
+        btnSubmit = findViewById(R.id._btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navToActivity();
+            }
+        });
+    }
+
+    private void navToActivity() {
+        Intent intent = new Intent(this, Main2Activity.class);
+        /**
+         *  Passing a message to another activity
+         */
+        intent.putExtra(EXTRA_MESSAGE, "Message comes here");
+        startActivity(intent);
+    }
+
+
 }
